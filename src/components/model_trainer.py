241b6c6e3d8ed67631337e9ec5832fe99c350fe9
Raghav Sharma <s.raghavsharma2012@gmail.com>
2023-08-01 18:23:42 +0530

    THe flask is runnign

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -62,8 +62,6 @@ class ModelTrainer:
                     # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
                     'learning_rate':[.1,.01,.05,.001],
                     'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "Linear Regression":{},
